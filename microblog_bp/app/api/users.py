5130b956b0a214cfd72895493714af536a9e0c84
Juan Camilo Valencia <jcvalencia22@hotmail.com>
2018-07-14 11:01:20 -0500

    Add files via upload

---
+++ microblog_bp/app/api/users.py
@@ -0,0 +1,25 @@
+from api import api
+
+@api.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    pass
+
+@api.route('/users', methods=['GET'])
+def get_users():
+    pass
+
+@api.route('/users/<int:id>/followers', methods=['GET'])
+def get_followers(id):
+    pass
+
+@api.route('/users/<int:id>/followed', methods=['GET'])
+def get_followed(id):
+    pass
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    pass
+
+@api.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    pass
